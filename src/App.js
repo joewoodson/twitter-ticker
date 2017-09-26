b34dccc8a1058b8156b5f165a6f168955e6fda9b
Joe Woodson <joe@appstretto.com>
2017-09-26 15:10:44 +0200

    create components folder and tweet component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Ticker from './ticker';
-import logo from './logo.svg';
+import Ticker from './components/ticker';
 import './App.css';
 
 class App extends Component {
--- src/components/ticker.js
+++ src/components/ticker.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-refetch'
+import Tweet from './tweet';
 
 const rootUrl = 'https://api.twitter.com/1.1/';
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -9,14 +10,18 @@ class Ticker extends Component {
     const { favoritesFetch } = this.props
 
     if (favoritesFetch.pending) {
-      // return <LoadingAnimation/>
-      return <h3>Loading</h3>
+      return <h3>Loading...</h3>
     } else if (favoritesFetch.rejected) {
       console.log(favoritesFetch.reason)
       return <h3>Error</h3>
     } else if (favoritesFetch.fulfilled) {
       console.log(favoritesFetch.value);
-      return <h3>{favoritesFetch.value.title}</h3>
+      const favorites = favoritesFetch.value.map((favorite) =>
+        <Tweet key={favorite.id_str} text={favorite.text} />
+      );
+
+      return <ul>{favorites}</ul>
+
     }
 
   }
@@ -27,6 +32,6 @@ export default connect(props => ({
     url: `${proxyUrl}${rootUrl}favorites/list.json?count=10&screen_name=joewdsn`,
     headers: {
       Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAC7k2QAAAAAAUGifZBfJhkrz2xTH6o4f0F0KQcA%3DIqMxALOukBJv8V77TeGVsuGxwxlTKu3B1S8KUW3628TN3RrNSt'
-    }
+    },
   }
 }))(Ticker)
