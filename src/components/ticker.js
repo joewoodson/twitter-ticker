b454bebbe330f2ccc1cc1747b28875b821bd187b
Joe Woodson <joe@appstretto.com>
2017-09-26 16:11:53 +0200

    add webticker jquery plugin

--- src/components/ticker.js
+++ src/components/ticker.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react'
 import { connect } from 'react-refetch'
 import Tweet from './tweet';
 
+declare var jQuery: jQuery;
+
 const rootUrl = 'https://api.twitter.com/1.1/';
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
 class Ticker extends Component {
+  componentDidMount(){
+    this.initWebTicker();
+  }
+
+  initWebTicker(){
+    jQuery(this.refs.webTicker).webTicker({
+      speed: 150,
+      hoverpause: false,
+    });
+  }
+
   render() {
     const { favoritesFetch } = this.props
+
+    return (
+      <ul id="webTicker" ref="webTicker">
+ 	    <li>This List Item will scroll infintely</li>
+ 	    <li>And this one will follow it</li>
+ 	    <li>Finally when it goes out of screen, it will queue again at the end</li>
+      </ul>
+    );
 
     if (favoritesFetch.pending) {
       return <h3>Loading...</h3>
@@ -15,7 +36,6 @@ class Ticker extends Component {
       console.log(favoritesFetch.reason)
       return <h3>Error</h3>
     } else if (favoritesFetch.fulfilled) {
-      console.log(favoritesFetch.value);
       const favorites = favoritesFetch.value.map((favorite) =>
         <Tweet key={favorite.id_str} text={favorite.text} />
       );
