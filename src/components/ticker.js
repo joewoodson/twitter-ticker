d91d5536759f0164a8dcda01f0de4b488bab08cb
Joe Woodson <joe@appstretto.com>
2017-09-29 10:46:00 +0200

    remove old ticker methods

--- src/components/ticker.js
+++ src/components/ticker.js
@@ -40,12 +40,12 @@ class Ticker extends Component {
       }
       return;
     } else {
-      // this.fetchTweets();
+      this.fetchTweets();
     }
 
-    // const intervalId = setInterval(this.fetchTweets.bind(this), 10000);
+    const intervalId = setInterval(this.fetchTweets.bind(this), 10000);
     // store intervalId in the state so it can be accessed later:
-    // this.setState({intervalId: intervalId});
+    this.setState({intervalId: intervalId});
   }
 
   componentWillUnmount(){
@@ -77,41 +77,6 @@ class Ticker extends Component {
     });
   }
 
-  updateTweets(updatedTweets){
-    console.log(updatedTweets);
-    this.updateWebTicker(updatedTweets);
-  }
-
-  updateWebTicker(favoritesList){
-    console.log('updating');
-    let favoritesHtml = '';
-    const updatedFavoritesList = favoritesList.map((favorite, i) => {
-
-      // remove urls from Tweets that include media
-      let text = favorite.full_text;
-      if (favorite.entities.urls[0]) {
-        text = text.replace(favorite.entities.urls[0].url, '');
-      }
-
-      if (favorite.extended_entities) {
-        text= text.replace(favorite.extended_entities.media[0].url, '');
-      }
-
-      return <Tweet key={favorite.id_str} itemNum={i+1} text={text} author={favorite.user.screen_name} profileImage={favorite.user.profile_image_url} mediaUrl={favorite.entities.media ? favorite.entities.media[0].media_url_https : null} />
-    });
-
-    for (let i = 0; i < updatedFavoritesList.length; i++) {
-      favoritesHtml += ReactDOMServer.renderToStaticMarkup(updatedFavoritesList[i]);
-    }
-
-    jQuery(this.refs.webTicker).webTicker('update',
-        favoritesHtml,
-        'swap',
-        true,
-        false
-    );
-  }
-
   fetchTweets(){
     const request = new Request(`${proxyUrl}${rootUrl}favorites/list.json?&tweet_mode=extended&screen_name=joewdsn&count=10`, {
     	headers: new Headers({
@@ -127,7 +92,6 @@ class Ticker extends Component {
         this.setState({ running: true });
         this.initWebTicker();
       }
-      this.updateTweets(j);
     }).catch((err) => {
       console.log('Error: ' + err);
     });
@@ -156,16 +120,7 @@ class Ticker extends Component {
     return (
       <div className="marquee">
         <ul className="tweets">
-          <li data-update="item1" className="tweet"><img src="http://pbs.twimg.com/profile_images/905479981459013637/a6BbKh4k_normal.jpg" alt="user profile"/> <span className="author">@nytimes:</span> <span className="text">The Interpreter: The unwritten rules for secession, and the contradictions among them </span></li>
-          <li data-update="item2" className="tweet"><img src="http://pbs.twimg.com/profile_images/782474226020200448/zDo-gAo0_normal.jpg" alt="user profile"/> <span className="author">@elonmusk:</span> <span className="text">Fly to most places on Earth in under 30 mins and anywhere in under 60. Cost per seat should be… </span></li>
-          <li data-update="item3" className="tweet"><img src="http://pbs.twimg.com/profile_images/905479981459013637/a6BbKh4k_normal.jpg" alt="user profile"/> <span className="author">@nytimes:</span> <span className="text">Myanmar abruptly cancelled a U.N. visit to the center of the Rohingya crisis, citing bad weather </span></li>
-          <li data-update="item4" className="tweet"><img src="http://pbs.twimg.com/profile_images/905479981459013637/a6BbKh4k_normal.jpg" alt="user profile"/> <span className="author">@nytimes:</span> <span className="text">Facing arrest, Catalonia&#x27;s &quot;accidental&quot; leader says his region will vote Sunday on independence from Spain </span></li>
-          <li data-update="item5" className="tweet"><img src="http://pbs.twimg.com/profile_images/671865418701606912/HECw8AzK_normal.jpg" alt="user profile"/> <span className="author">@SpaceX:</span> <span className="text">First opportunity to land BFR with cargo on Mars is 2022, followed by BFR crew and cargo missions to Mars in 2024. </span></li>
-          <li data-update="item6" className="tweet"><img src="http://pbs.twimg.com/profile_images/877554927932891136/ZBEs235N_normal.jpg" alt="user profile"/> <span className="author">@Reuters:</span> <span className="text">FCC chair wants Apple to activate FM radio chips in iPhones  </span><img className="image-attached" src="https://pbs.twimg.com/media/DK0mBoLXcAASu9o.jpg" alt="attachment"/></li>
-          <li data-update="item7" className="tweet"><img src="http://pbs.twimg.com/profile_images/903636921175252992/K9WYH-E2_normal.jpg" alt="user profile"/> <span className="author">@wizards_magic:</span> <span className="text">Who among you is excited for #MTGXLN’s release? Then, like Huatli, write a poem, and let your awesomeness increase! (On Sale September 29th) </span><img className="image-attached" src="https://pbs.twimg.com/media/DK0fI5FXkAA2G0-.png" alt="attachment"/></li>
-          <li data-update="item8" className="tweet"><img src="http://pbs.twimg.com/profile_images/872153172256505857/CKtR_6lP_normal.jpg" alt="user profile"/> <span className="author">@shovan_ch:</span> <span className="text">Got my PR merged with @freeCodeCamp. Feeling great . Looking frward to contribute more #100DaysOfCode #CodeNewbie  </span><img className="image-attached" src="https://pbs.twimg.com/media/DKtcp24VwAEppKV.jpg" alt="attachment"/></li>
-          <li data-update="item9" className="tweet"><img src="http://pbs.twimg.com/profile_images/861698928311271424/lqDIYF8B_normal.jpg" alt="user profile"/> <span className="author">@lupecamach0:</span> <span className="text">It&#x27;s official. I&#x27;m starting my very first #webdeveloper job mid-October. Thank you @freeCodeCamp and @TheOdinProject and @treehouse!</span></li>
-          <li data-update="item10" className="tweet"><img src="http://pbs.twimg.com/profile_images/378800000147359764/54dc9a5c34e912f34db8662d53d16a39_normal.png" alt="user profile"/> <span className="author">@ossia:</span> <span className="text">The freeCodeCamp community YouTube channel won a Silver Play Button award from YouTube! Watch me unbox it. </span></li>
+          {favoritesList}
         </ul>
       </div>
     )
