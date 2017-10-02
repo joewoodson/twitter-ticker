2aa63e5dc88f3bdf039c3cea6d82942a2469967d
Joe Woodson <joe@appstretto.com>
2017-10-02 12:57:37 +0200

    move endlessriver ticker logic inside of ticker component

--- src/components/tweet.js
+++ src/components/tweet.js
@@ -4,7 +4,7 @@ import '../css/Tweet.css';
 class Tweet extends Component {
   render() {
     return (
-      <li data-update={`item${this.props.itemNum}`} className="tweet">
+      <li data-update={`item${this.props.itemNum}`} id={this.props.elemId} className={this.props.isFirst ? "first tweet" : "tweet"}>
         <img src={this.props.profileImage} alt="" /> <span className="author">@{this.props.author}:</span> <span className="text">{this.props.text}</span>
         { this.props.mediaUrl &&
           <img className="image-attached" src={this.props.mediaUrl} alt="" />
